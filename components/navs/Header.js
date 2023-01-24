976fb5a6f79e536c5cfa835945a2adee0b7a01a8
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-24 12:23:33 -0400

    🚸 add dark mode icon

--- components/navs/Header.js
+++ components/navs/Header.js
@@ -13,7 +13,11 @@ export default function Header(){
                     <a href="#" className='ml-5 hover:text-slate-900'>Generador de gradientes</a>
                     <span className="ml-5">|</span>
                     <a href="#" className='ml-5 hover:text-slate-900'>Github</a>
-                    <span className='ml-5 hover:text-slate-900'>Modo Oscuro</span>
+                    <span className='ml-5 hover:text-slate-900'>
+                        <svg className="w-6" fill="none" stroke="currentColor" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                        </svg>
+                    </span>
                 </nav>
             </div>
         </header>
