e2104b0fccb5df97aad22ad5b8c6f0c4d4185d6f
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-24 12:06:53 -0400

    🚸 text color

--- components/navs/Header.js
+++ components/navs/Header.js
@@ -7,13 +7,13 @@ export default function Header(){
         <header className='container mx-auto py-2'>
             <div className="flex justify-between items-center">
                 <Image src="/images/logotipo.png" alt="Logotipo de tailwindflow" width={ 200 } height={ 200 } />
-                <nav className='flex'>
-                    <a href="#" className='ml-5'>Inicio</a>
-                    <a href="#" className='ml-5'>Componentes</a>
-                    <a href="#" className='ml-5'>Generador de gradientes</a>
+                <nav className='flex text-slate-700'>
+                    <a href="#" className='ml-5 hover:text-slate-900'>Inicio</a>
+                    <a href="#" className='ml-5 hover:text-slate-900'>Componentes</a>
+                    <a href="#" className='ml-5 hover:text-slate-900'>Generador de gradientes</a>
                     <span className="ml-5">|</span>
-                    <a href="#" className='ml-5'>Github</a>
-                    <span className='ml-5'>Modo Oscuro</span>
+                    <a href="#" className='ml-5 hover:text-slate-900'>Github</a>
+                    <span className='ml-5 hover:text-slate-900'>Modo Oscuro</span>
                 </nav>
             </div>
         </header>
