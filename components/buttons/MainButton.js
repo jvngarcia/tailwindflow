a749cddba4b48766bdcdba898d1a8359055ae638
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-24 12:22:42 -0400

    🎨 add extra className

--- components/buttons/MainButton.js
+++ components/buttons/MainButton.js
@@ -1,7 +1,7 @@
 
-export default function MainButton({ children }){
+export default function MainButton({ className = '', children }){
     return (
-        <button className="bg-indigo-600 hover:bg-indigo-700 text-white py-3 px-9 rounded">
+        <button className={ `bg-indigo-600 hover:bg-indigo-700 text-white py-3 px-9 rounded ${ className }` }>
             {children}
         </button>
     )
