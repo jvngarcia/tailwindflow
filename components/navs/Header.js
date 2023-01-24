3877838ae2df67e210161e59146a3ac85203fa13
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-24 12:42:04 -0400

    💄 align items center

--- components/navs/Header.js
+++ components/navs/Header.js
@@ -7,7 +7,7 @@ export default function Header(){
         <header className='container mx-auto py-2'>
             <div className="flex justify-between items-center">
                 <Image src="/images/logotipo.png" alt="Logotipo de tailwindflow" width={ 200 } height={ 200 } />
-                <nav className='flex text-slate-700'>
+                <nav className='flex items-center text-slate-700'>
                     <a href="#" className='ml-5 hover:text-slate-900'>Inicio</a>
                     <a href="#" className='ml-5 hover:text-slate-900'>Componentes</a>
                     <a href="#" className='ml-5 hover:text-slate-900'>Generador de gradientes</a>
