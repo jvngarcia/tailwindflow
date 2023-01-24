6bf8127d1a616801f802301e269953b7a6e46530
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-24 11:02:25 -0400

    First Config

--- pages/_document.js
+++ pages/_document.js
@@ -2,12 +2,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="es">
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
+      <footer>
+        <p>Footer</p>
+      </footer>
     </Html>
   )
 }
