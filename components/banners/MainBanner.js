6c2b708465f65708859d60f539040fbb53c77af1
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-24 12:29:27 -0400

    💄 font weight bold

--- components/banners/MainBanner.js
+++ components/banners/MainBanner.js
@@ -4,7 +4,7 @@ import MainButton from "../buttons/MainButton";
 export default function MainBanner({ className = '', title, subTitle, textButton, linkButton }){
     return (
         <div className={ `flex flex-col items-center justify-center ${ className }` }>
-            <h1 className='text-5xl text-slate-700 text-center w-2/4'>{ title }</h1>
+            <h1 className='text-5xl text-slate-700 text-center w-2/4 font-bold'>{ title }</h1>
             <p className='text-slate-700 text-center mt-4 text-xl'>{ subTitle }</p>
             <MainButton className="mt-7">{ textButton }</MainButton>
         </div>
