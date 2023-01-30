a6e76b73028a806e76d1de81bf71547a81f27037
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-30 16:47:48 -0400

    🐛 detect button existence

--- components/banners/MainBanner.js
+++ components/banners/MainBanner.js
@@ -2,11 +2,16 @@ import MainLink from "../links/MainLink";
 
 
 export default function MainBanner({ className = '', title, subTitle, textButton, linkButton }){
+
     return (
         <div className={ `flex flex-col items-center justify-center ${ className }` }>
             <h1 className='text-5xl text-slate-700 text-center w-3/4 font-bold'>{ title }</h1>
             <p className='text-slate-700 text-center mt-4 text-xl'>{ subTitle }</p>
-            <MainLink href={ linkButton } className="mt-7">{ textButton }</MainLink>
+
+            { textButton && 
+                <MainLink href={ linkButton } className="mt-7">{ textButton }</MainLink>
+            }
+
         </div>
     )
 }
