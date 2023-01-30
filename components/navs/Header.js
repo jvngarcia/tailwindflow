1444d20c223b64afc97f7af752fe2fb3dae0add7
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-30 15:16:39 -0400

    💄 remove extra links

--- components/navs/Header.js
+++ components/navs/Header.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 
 
@@ -6,10 +7,11 @@ export default function Header(){
     return (
         <header className='container mx-auto py-2'>
             <div className="flex justify-between items-center">
-                <Image src="/images/logotipo.png" alt="Logotipo de tailwindflow" width={ 200 } height={ 200 } />
+                <Link href="/">
+                    <Image src="/images/logotipo.png" alt="Logotipo de tailwindflow" width={ 200 } height={ 200 } />
+                </Link>
                 <nav className='flex items-center text-slate-700'>
-                    <a href="#" className='ml-5 hover:text-slate-900'>Inicio</a>
-                    <a href="#" className='ml-5 hover:text-slate-900'>Componentes</a>
+                    <Link href="/#componentes" className='ml-5 hover:text-slate-900'>Componentes</Link>
                     <a href="#" className='ml-5 hover:text-slate-900'>Generador de gradientes</a>
                     <span className="ml-5">|</span>
                     <a href="#" className='ml-5 hover:text-slate-900 flex items-center'>
