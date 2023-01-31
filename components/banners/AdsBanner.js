ebe7e7946c1a992539e409a025e9c9f5441be9bc
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-31 16:26:38 -0400

    💄 update style

--- components/banners/AdsBanner.js
+++ components/banners/AdsBanner.js
@@ -2,11 +2,13 @@
 
 export default function AdsBanner({ title, subTitle, textButton, linkButton }){
     return (
-        <div className='bg-indigo-200 py-12 rounded-md'>
-            <div className='container mx-auto flex flex-col items-center justify-center py-6'>
+        <div className='bg-indigo-200 py-12 rounded-md px-8'>
+            <div className='container py-6'>
                 <h2 className='text-3xl text-slate-700 font-bold'>{ title }</h2>   
-                <p className='text-slate-700 text-center mt-4 text-xl'>{ subTitle }</p>
-                <a href={ linkButton } target="_blank" className='mt-7 bg-slate-700 text-white py-3 px-6 rounded-lg hover:bg-slate-900'>{ textButton }</a>
+                <p className='text-slate-700 mt-4 text-xl'>{ subTitle }</p>
+                <div className="mt-7">
+                    <a href={ linkButton } target="_blank" className='mt-12 bg-slate-700 text-white py-3 px-6 rounded-lg hover:bg-slate-900'>{ textButton }</a>
+                </div>
             </div>
         </div>
     )
