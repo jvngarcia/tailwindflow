7b01599971ca6f7c2db33c2c9dcba1c307766063
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-31 15:03:07 -0400

    🐛 error key

--- components/grids/MainCardsGrid.js
+++ components/grids/MainCardsGrid.js
@@ -6,7 +6,7 @@ export default function MainCardsGrid({ components, id = "componentes" }){
     return (
         <div id={ id } className='grid lg:grid-cols-4 md:grid-cols-3 mt-6 gap-6'>
             { components && components.map( (component, index) => ( 
-                <Link href={ component.slug }>
+                <Link key={ index } href={ component.slug }>
                     <MainCard key={index} mainImage={component.image} title={component.name} profileName="JVN García" profileImage="/tailwindflow-favicon.png" />
                 </Link>
             ))}
