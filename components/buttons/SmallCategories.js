ec6fea6f7eb1dedb7a67b55f056a2d64ab455c8a
Angel Garcia <angelgarciaweb@gmail.com>
2023-01-31 15:02:39 -0400

    💄 change color

--- components/buttons/SmallCategories.js
+++ components/buttons/SmallCategories.js
@@ -1,9 +1,9 @@
 
 
-export default function SmallCategories({ children }){
+export default function SmallCategories({ children, className }){
     return (
-        <div className='bg-indigo-100 rounded-md'>
-            <h6 className='text-indigo-600 text-center py-3 font-medium'>{ children }</h6>
+        <div className={ `bg-indigo-200 rounded-md ${ className }` }>
+            <h6 className='text-indigo-700 text-center py-3 font-medium'>{ children }</h6>
         </div>
     )
 }
--- components/cards/MainCard.js
+++ components/cards/MainCard.js
@@ -4,11 +4,11 @@ import Image from "next/image";
 export default function MainCard({ mainImage, title, profileImage, profileName }){
     return (
         <div>
-            <div className='overflow-hidden w-full h-48 bg-indigo-100 flex items-center justify-center rounded-md'>
+            <div className='overflow-hidden w-full h-48 bg-indigo-200 flex items-center justify-center rounded-md'>
                 <Image src={ mainImage } width={230} height={230} className="object-cover" alt={ title } />
             </div>
             <div className='flex mt-3 items-center'>
-                <div className='rounded-full w-10 h-10 bg-indigo-100 overflow-hidden flex justify-center items-center'>
+                <div className='rounded-full w-10 h-10 bg-indigo-200 overflow-hidden flex justify-center items-center'>
                     <Image src={ profileImage } width={100} height={100} className="object-cover" alt={ profileName } />
                 </div>
                 <div className='ml-3 w-full truncate'>
