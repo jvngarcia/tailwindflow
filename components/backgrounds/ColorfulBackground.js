71895e85287c9509cb3d5b1cfe9e8b951de15820
Angel Garcia <angelgarciaweb@gmail.com>
2023-02-12 13:55:51 -0400

    🍱 update background

--- components/backgrounds/ColorfulBackground.js
+++ components/backgrounds/ColorfulBackground.js
@@ -4,8 +4,6 @@ import Image from "next/image";
 
 export default function ColorfulBackground(){
     return (
-        <div className="-z-50 absolute w-full top-0 left-0 right-0 bottom-0">
-          <Image src="/images/main-background.png" width={100} height={100} className="object-fill w-full max-h-full" alt="background image" />
-        </div>
+        <div className="-z-50 absolute w-full top-0 left-0 right-0 bottom-0" id="main-background"></div>
     )
 }
